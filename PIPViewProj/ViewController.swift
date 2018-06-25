9cddccdc9e254ed046b2979307d7c8ebe60d54e0
Samuel Lee <slee@sleela.local>
2018-06-25 10:12:11 -0700

    Initial Commit

---
+++ PIPViewProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PIPViewProj
+//
+//  Created by Samuel Lee on 6/25/18.
+//  Copyright © 2018 Samuel Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PIPViewProjTests/PIPViewProjTests.swift
@@ -0,0 +1,36 @@
+//
+//  PIPViewProjTests.swift
+//  PIPViewProjTests
+//
+//  Created by Samuel Lee on 6/25/18.
+//  Copyright © 2018 Samuel Lee. All rights reserved.
+//
+
+import XCTest
+@testable import PIPViewProj
+
+class PIPViewProjTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
