f79c5ed42e9e519f2c022f69b85e29b882fc3d2d
Samuel Lee <slee@sleela.local>
2018-06-27 16:35:06 -0700

    fixed warning

--- PIPViewProj/ViewController.swift
+++ PIPViewProj/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var testView = View(frame: self.view.frame)
+        let testView = View(frame: self.view.frame)
         self.view.addSubview(testView)
 
     }
